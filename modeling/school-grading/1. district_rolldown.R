524f8be4feffbcc5a5bdd55a738b99dd7f9f5eec
Alexander Poon <alexander.poon@vanderbilt.edu>
2016-09-15 14:52:14 -0500

    rematched model 1

--- modeling/school-grading/1. district_rolldown.R
+++ modeling/school-grading/1. district_rolldown.R
@@ -59,7 +59,7 @@ ach_heat_map <- school_accountability %>%
         tvaas_goal = ifelse(TVAAS_level == "Level 4", 3, tvaas_goal),
         tvaas_goal = ifelse(TVAAS_level == "Level 5", 4, tvaas_goal)) %>%
     rowwise() %>%
-    mutate(average_score = ifelse(eligible, mean(c(amo_targets_goal, relative_performance_goal, tvaas_goal), na.rm = TRUE), NA)) %>%
+    mutate(average_score = ifelse(eligible, round(mean(c(amo_targets_goal, relative_performance_goal, tvaas_goal), na.rm = TRUE), 2), NA)) %>%
     ungroup() %>%
     select(system, system_name, school, school_name, subject, pool, eligible, amo_targets_goal, relative_performance_goal, tvaas_goal, average_score)
 
@@ -67,30 +67,25 @@ ach_heat_map <- school_accountability %>%
 subgroup_heat_maps <- school_accountability %>%
     filter(subgroup %in% c("Black/Hispanic/Native American", "Economically Disadvantaged", "Students with Disabilities", "English Language Learners with T1/T2")) %>%
     mutate(eligible = (valid_tests >= 30 & valid_tests_prior >= 30),
-       amo_targets_goal = ifelse(eligible, ifelse(upper_bound_ci_PA <= pct_prof_adv_prior, 0, NA), NA),
-       amo_targets_goal = ifelse(eligible, ifelse(upper_bound_ci_PA > pct_prof_adv_prior & upper_bound_ci_PA < AMO_target_PA, 1, amo_targets_goal), NA),
-       amo_targets_goal = ifelse(eligible, ifelse(upper_bound_ci_PA >= AMO_target_PA & pct_prof_adv <= AMO_target_PA, 2, amo_targets_goal), NA),
-       amo_targets_goal = ifelse(eligible, ifelse(pct_prof_adv > AMO_target_PA & pct_prof_adv < AMO_target_PA_4, 3, amo_targets_goal), NA),
-       amo_targets_goal = ifelse(eligible, ifelse(pct_prof_adv >= AMO_target_PA_4, 4, amo_targets_goal), NA),
-       tvaas_goal = ifelse(eligible, ifelse(TVAAS_level == "Level 1", 0, NA), NA),
-       tvaas_goal = ifelse(eligible, ifelse(TVAAS_level == "Level 2", 1, tvaas_goal), NA),
-       tvaas_goal = ifelse(eligible, ifelse(TVAAS_level == "Level 3", 2, tvaas_goal), NA),
-       tvaas_goal = ifelse(eligible, ifelse(TVAAS_level == "Level 4", 3, tvaas_goal), NA),
-       tvaas_goal = ifelse(eligible, ifelse(TVAAS_level == "Level 5", 4, tvaas_goal), NA),
-       bb_reduction_goal = ifelse(eligible, ifelse(lower_bound_ci_BB >= pct_below_bsc_prior, 0, NA), NA),
-       bb_reduction_goal = ifelse(eligible, ifelse(lower_bound_ci_BB < pct_below_bsc_prior & lower_bound_ci_BB > AMO_target_BB, 1, bb_reduction_goal), NA),
-       bb_reduction_goal = ifelse(eligible, ifelse(lower_bound_ci_BB <= AMO_target_BB & pct_below_bsc >= AMO_target_BB, 2, bb_reduction_goal), NA),
-       bb_reduction_goal = ifelse(eligible, ifelse(pct_below_bsc < AMO_target_BB & pct_below_bsc > AMO_target_BB_4, 3, bb_reduction_goal), NA),
-       bb_reduction_goal = ifelse(eligible, ifelse(pct_below_bsc <= AMO_target_BB_4, 4, bb_reduction_goal), NA)) %>% 
+        amo_targets_goal = ifelse(eligible, ifelse(upper_bound_ci_PA <= pct_prof_adv_prior, 0, NA), NA),
+        amo_targets_goal = ifelse(eligible, ifelse(upper_bound_ci_PA > pct_prof_adv_prior & upper_bound_ci_PA < AMO_target_PA, 1, amo_targets_goal), NA),
+        amo_targets_goal = ifelse(eligible, ifelse(upper_bound_ci_PA >= AMO_target_PA & pct_prof_adv <= AMO_target_PA, 2, amo_targets_goal), NA),
+        amo_targets_goal = ifelse(eligible, ifelse(pct_prof_adv > AMO_target_PA & pct_prof_adv < AMO_target_PA_4, 3, amo_targets_goal), NA),
+        amo_targets_goal = ifelse(eligible, ifelse(pct_prof_adv >= AMO_target_PA_4, 4, amo_targets_goal), NA),
+        bb_reduction_goal = ifelse(eligible, ifelse(lower_bound_ci_BB >= pct_below_bsc_prior, 0, NA), NA),
+        bb_reduction_goal = ifelse(eligible, ifelse(lower_bound_ci_BB < pct_below_bsc_prior & lower_bound_ci_BB > AMO_target_BB, 1, bb_reduction_goal), NA),
+        bb_reduction_goal = ifelse(eligible, ifelse(lower_bound_ci_BB <= AMO_target_BB & pct_below_bsc >= AMO_target_BB, 2, bb_reduction_goal), NA),
+        bb_reduction_goal = ifelse(eligible, ifelse(pct_below_bsc < AMO_target_BB & pct_below_bsc > AMO_target_BB_4, 3, bb_reduction_goal), NA),
+        bb_reduction_goal = ifelse(eligible, ifelse(pct_below_bsc <= AMO_target_BB_4, 4, bb_reduction_goal), NA)) %>% 
     rowwise() %>%
-    mutate(average_score = ifelse(eligible, mean(c(amo_targets_goal, tvaas_goal, bb_reduction_goal), na.rm = TRUE), NA)) %>%
+    mutate(average_score = ifelse(eligible, mean(c(amo_targets_goal, bb_reduction_goal), na.rm = TRUE), NA)) %>%
     ungroup() %>%
-    select(system, system_name, school, school_name, subgroup, pool, subject, eligible, amo_targets_goal, tvaas_goal, bb_reduction_goal, average_score)
+    select(system, system_name, school, school_name, subgroup, pool, subject, eligible, amo_targets_goal, bb_reduction_goal, average_score)
 
 # Achievement Scores
 ach_scores <- ach_heat_map %>%
     group_by(system, system_name, school, school_name, pool) %>%
-    summarise(achievement_score = mean(average_score, na.rm = TRUE)) %>%
+    summarise(achievement_score = round(mean(average_score, na.rm = TRUE), 2)) %>%
     mutate(achievement_determination = ifelse(achievement_score < 2, "Progressing", NA),
         achievement_determination = ifelse(achievement_score >= 2 & achievement_score < 3, "Achieving", achievement_determination),
         achievement_determination = ifelse(achievement_score >= 3, "Exemplary", achievement_determination))
@@ -98,9 +93,9 @@ ach_scores <- ach_heat_map %>%
 # Overall Gap Scores
 gap_scores <- subgroup_heat_maps %>%
     group_by(system, system_name, school, school_name, subgroup, pool) %>%
-    summarise(subgroup_average = mean(average_score, na.rm = TRUE)) %>%
+    summarise(subgroup_average = round(mean(average_score, na.rm = TRUE), 2)) %>%
     group_by(system, system_name, school, school_name, pool) %>%
-    summarise(gap_score = mean(subgroup_average, na.rm = TRUE)) %>%
+    summarise(gap_score = round(mean(subgroup_average, na.rm = TRUE), 2)) %>%
     mutate(gap_determination = ifelse(gap_score < 2, "Progressing", NA),
         gap_determination = ifelse(gap_score >= 2 & gap_score < 3, "Achieving", gap_determination),
         gap_determination = ifelse(gap_score >= 3, "Exemplary", gap_determination))
@@ -111,13 +106,19 @@ final_determinations <- minimum_performance_goal %>%
     full_join(ach_scores, by = c("system", "system_name", "school", "school_name", "pool")) %>%
     full_join(gap_scores, by = c("system", "system_name", "school", "school_name", "pool")) %>%
     rowwise() %>%
-    mutate(overall_average = mean(c(achievement_score, gap_score), na.rm = TRUE)) %>%
+    mutate(overall_average = round(mean(c(achievement_score, gap_score), na.rm = TRUE), 2)) %>%
     ungroup() %>%
     mutate(final_determination = ifelse(minimum_performance_goal == "Missed", "In Need of Improvement", NA),
         final_determination = ifelse(is.na(final_determination), ifelse(overall_average < 2, "Progressing", final_determination), final_determination),
         final_determination = ifelse(is.na(final_determination), ifelse(overall_average >= 2 & overall_average < 3, "Achieving", final_determination), final_determination),
         final_determination = ifelse(is.na(final_determination), ifelse(overall_average >= 3, "Exemplary", final_determination), final_determination),
-        final_determination = ifelse(designation_ineligible, NA, final_determination))
+        final_determination = ifelse(designation_ineligible, NA, final_determination),
+        final_grade = ifelse(minimum_performance_goal == "Missed", "F", NA),
+        final_grade = ifelse(is.na(final_grade), ifelse(overall_average < 1, "D", final_grade), final_grade),
+        final_grade = ifelse(is.na(final_grade), ifelse(overall_average >= 1 & overall_average < 2, "C", final_grade), final_grade),
+        final_grade = ifelse(is.na(final_grade), ifelse(overall_average >= 2 & overall_average < 3, "B", final_grade), final_grade),
+        final_grade = ifelse(is.na(final_grade), ifelse(overall_average >= 3, "A", final_grade), final_grade),
+        final_grade = ifelse(designation_ineligible, NA, final_grade))
 
 # Output file
 write_csv(final_determinations, path = "data/district_rolldown_determinations.csv", na = "")
