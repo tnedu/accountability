45029d776df14958cc1e2a7dac34d51fc7f47c2d
Alexander Poon <alexander.poon@vanderbilt.edu>
2017-03-07 16:16:47 -0600

    fix tvaas calculation

--- modeling/district-accountability/0. grade_band_tvaas.R
+++ modeling/district-accountability/0. grade_band_tvaas.R
@@ -14,14 +14,14 @@ tvaas <- readxl::read_excel("K:/ORP_accountability/data/2015_tvaas/District-Leve
         Grade = ifelse(Test == "EOC", "9-12", Grade),
         temp = 1) %>% 
     group_by(`System Number`, System, Year, Subgroup, Grade) %>% 
-    summarise(index = mean(Index, na.rm = TRUE), n_subjects = sum(temp, na.rm = TRUE)) %>%
+    summarise(index_avg = mean(Index, na.rm = TRUE), n_subjects = sum(temp, na.rm = TRUE)) %>%
     ungroup() %>%
-    mutate(Index = index * sqrt(n_subjects),
-        tvaas_level = ifelse(index < -2, "Level 1", NA),
-        tvaas_level = ifelse(index >= -2 & index < -1, "Level 2", tvaas_level),
-        tvaas_level = ifelse(index >= -1 & index < 1, "Level 3", tvaas_level),
-        tvaas_level = ifelse(index >= 1 & index < 2, "Level 4", tvaas_level),
-        tvaas_level = ifelse(index >= 2, "Level 5", tvaas_level)) %>%
+    mutate(Index = index_avg * sqrt(n_subjects),
+        tvaas_level = ifelse(Index < -2, "Level 1", NA),
+        tvaas_level = ifelse(Index >= -2, "Level 2", tvaas_level),
+        tvaas_level = ifelse(Index >= -1, "Level 3", tvaas_level),
+        tvaas_level = ifelse(Index >= 1, "Level 4", tvaas_level),
+        tvaas_level = ifelse(Index >= 2, "Level 5", tvaas_level)) %>%
     mutate_at("Grade", funs(recode(.,
         "4-5" = "3-5 Success Rate",
         "6-8" = "6-8 Success Rate",
