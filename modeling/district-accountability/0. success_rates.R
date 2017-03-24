cce962c8191b1fc046b098ae0707e5fc04ecd574
Alexander Poon <alexander.poon@vanderbilt.edu>
2017-03-24 16:49:55 -0500

    add ACT to success rates

--- modeling/district-accountability/0. success_rates.R
+++ modeling/district-accountability/0. success_rates.R
@@ -1,7 +1,23 @@
 ## Success and Below Basic Rates + TVAAS for District Accountability
 
+library(haven)
 library(readxl)
 library(tidyverse)
+
+# ACT for Success Rates (with all test takers as denominator)
+ACT_prior <- read_dta("K:/ORP_accountability/data/2015_ACT/ACT_district2015.dta") %>%
+    filter(subgroup %in% c("All Students", "Black/Hispanic/Native American", "Economically Disadvantaged",
+        "Students with Disabilities", "English Language Learners with T1/T2", "Super Subgroup")) %>%
+    transmute(year = 2014, system, system_name, subject = "ACT Composite",
+        subgroup = ifelse(subgroup == "English Language Learners with T1/T2", "English Learners", subgroup),
+        valid_tests = valid_tests_nogradcohort, n_prof = num_21_orhigher_nogradcohort)
+
+ACT <- read_dta("K:/ORP_accountability/data/2015_ACT/ACT_district2016.dta") %>%
+    filter(subgroup %in% c("All Students", "Black/Hispanic/Native American", "Economically Disadvantaged",
+        "Students with Disabilities", "English Language Learners with T1/T2", "Super Subgroup")) %>%
+    transmute(year = 2015, system, system_name, subject = "ACT Composite",
+        subgroup = ifelse(subgroup == "English Language Learners with T1/T2", "English Learners", subgroup),
+        valid_tests = valid_tests_nogradcohort, n_prof = num_21_orhigher_nogradcohort)
 
 # Apply suppression to base
 system_base <- read_csv("K:/ORP_accountability/projects/2016_pre_coding/Output/system_base_with_super_subgroup_2016.csv") %>%
@@ -15,32 +31,34 @@ system_base <- read_csv("K:/ORP_accountability/projects/2016_pre_coding/Output/s
         subject = ifelse(subject %in% c("Algebra I", "Algebra II") & grade <= 8, "Math", subject),
         subject = ifelse(subject %in% c("English I", "English II", "English III") & grade <= 8, "ELA", subject),
         subject = ifelse(subject %in% c("Biology I", "Chemistry") & grade <= 8, "Science", subject),
-        subject = ifelse(grade %in%c(3, 4, 5), paste("3-5", subject), subject),
-        subject = ifelse(grade %in% c(6, 7, 8), paste("6-8", subject), subject)) %>%
+        subject = ifelse(grade %in% 3:5, paste("3-5", subject), subject),
+        subject = ifelse(grade %in% 6:8, paste("6-8", subject), subject)) %>%
 # Aggregate across grades
     group_by(year, system, system_name, subject, subgroup) %>%
     summarise_each(funs(sum(., na.rm = TRUE)), valid_tests, n_below_bsc, n_bsc, n_prof, n_adv) %>%
     ungroup() %>%
 # Suppress below 30 at subject level
+    bind_rows(ACT_prior, ACT) %>%
     mutate_each(funs(ifelse(valid_tests < 30, 0, .)), valid_tests, n_below_bsc, n_bsc, n_prof, n_adv)
 
 # Calculate percent PA/Below with CI
 percent_PA_BB <- system_base %>%
     mutate(subject = ifelse(subject %in% c("3-5 Math", "3-5 ELA", "3-5 Science"), "3-5 Success Rate", subject),
         subject = ifelse(subject %in% c("6-8 Math", "6-8 ELA", "6-8 Science"), "6-8 Success Rate", subject),
-        subject = ifelse(subject %in% c("Algebra I", "Algebra II", "English I", "English II", "English III", "Biology I", "Chemistry"), "HS Success Rate", subject)) %>%
+        subject = ifelse(subject %in% c("Algebra I", "Algebra II", "English I", "English II", "English III", "Biology I", "Chemistry", "ACT Composite"), "HS Success Rate", subject)) %>%
     group_by(year, system, system_name, subject, subgroup) %>%
     summarise_each(funs(sum(., na.rm = TRUE)), valid_tests, n_below_bsc, n_bsc, n_prof, n_adv) %>%
     ungroup() %>%
-    mutate(pct_bsc = round(100 * n_bsc/valid_tests, 1),
-        pct_prof = round(100 * n_prof/valid_tests, 1),
-        pct_adv = round(100 * n_adv/valid_tests, 1),
+    mutate(
     # Percent PA and Upper Bound CI
         pct_prof_adv = round((n_prof + n_adv)/valid_tests, 3),
         upper_bound_ci_PA = round(100 * valid_tests/(valid_tests + qnorm(0.975)^2) * (pct_prof_adv + (qnorm(0.975)^2/(2 * valid_tests)) +
             qnorm(0.975) * sqrt((pct_prof_adv * (1 - pct_prof_adv))/valid_tests + qnorm(0.975)^2/(4 * valid_tests^2))), 1),
         pct_prof_adv = 100 * pct_prof_adv,
     # Percent BB and Lower Bound CI
+        pct_bsc = round(100 * n_bsc/valid_tests, 1),
+        pct_prof = round(100 * n_prof/valid_tests, 1),
+        pct_adv = round(100 * n_adv/valid_tests, 1),
         pct_below_bsc = round(100 - pct_bsc - pct_prof - pct_adv, 1)/100,
         lower_bound_ci_BB = round(100 * valid_tests/(valid_tests + qnorm(0.975)^2) * (pct_below_bsc + (qnorm(0.975)^2/(2 * valid_tests)) -
             qnorm(0.975) * sqrt((pct_below_bsc * (1 - pct_below_bsc))/valid_tests + qnorm(0.975)^2/(4 * valid_tests^2))), 1),
@@ -56,6 +74,7 @@ AMOs <- percent_PA_BB %>%
         AMO_target_PA = ifelse(valid_tests >= 30, round(pct_prof_adv + (100 - pct_prof_adv)/16, 1), NA),
         AMO_target_PA_4 = ifelse(valid_tests >= 30, round(pct_prof_adv + (100 - pct_prof_adv)/8, 1), NA))
 
+# Grade band TVAAS
 TVAAS <- read_excel("K:/ORP_accountability/data/2015_tvaas/TNTDE-2015-All-Students-Across-Subjects-20170314.xlsx") %>%
     bind_rows(read_excel("K:/ORP_accountability/data/2015_tvaas/TNTDE-2015-Subgroup-Measures-Across-Subjects-20170313.xlsx")) %>%
     mutate(Subgroup = ifelse(Subgroup == "English Language Learners", "English Learners", Subgroup),
