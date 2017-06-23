e5e99633e7e020f46fd94b38f4d001c761268130
alexander-poon <alexander.poon@vanderbilt.edu>
2017-06-23 14:49:52 -0500

    rematched initial cdf

--- 2017-accountability/1. cdf_to_student_level.R
+++ 2017-accountability/1. cdf_to_student_level.R
@@ -6,7 +6,7 @@ fall_cdf <- read_csv("K:/ORP_accountability/projects/2017_dictionary_coding/fall
         semester = "Fall",
         ri_status = if_else(ri_status_part_1 %in% c(0, 5), ri_status_part_2, ri_status_part_1),
         ri_status = if_else(is.na(ri_status), ri_status_part_1, ri_status),
-        absent = as.numeric(ri_status  == 5),
+        absent = as.numeric(ri_status == 5),
         did_not_attempt = as.numeric(ri_status == 4),
         residential_facility = as.numeric(ri_status == 1),
         nullify_flag = as.numeric(ri_status == 2),
@@ -47,10 +47,10 @@ student_level <- bind_rows(fall_cdf) %>%
         economically_disadvantaged = if_else(economically_disadvantaged == 2, 0L, economically_disadvantaged),
         el_t1_t2 = if_else(el_t1_t2 == 2, 1L, el_t1_t2),
         special_ed = as.numeric(special_ed %in% c(1, 2, 3)),
-        original_proficiency_level = if_else(original_subject %in% c(english_eoc, math_eoc) & performance_level == 1, "1. Below", NA_character_),
-        original_proficiency_level = if_else(original_subject %in% c(english_eoc, math_eoc) & performance_level == 2, "2. Approaching", original_proficiency_level),
-        original_proficiency_level = if_else(original_subject %in% c(english_eoc, math_eoc) & performance_level == 3, "3. On Track", original_proficiency_level),
-        original_proficiency_level = if_else(original_subject %in% c(english_eoc, math_eoc) & performance_level == 4, "4. Mastered", original_proficiency_level),
+        original_proficiency_level = if_else(original_subject %in% c(english_eoc, math_eoc, "US History") & performance_level == 1, "1. Below", NA_character_),
+        original_proficiency_level = if_else(original_subject %in% c(english_eoc, math_eoc, "US History") & performance_level == 2, "2. Approaching", original_proficiency_level),
+        original_proficiency_level = if_else(original_subject %in% c(english_eoc, math_eoc, "US History") & performance_level == 3, "3. On Track", original_proficiency_level),
+        original_proficiency_level = if_else(original_subject %in% c(english_eoc, math_eoc, "US History") & performance_level == 4, "4. Mastered", original_proficiency_level),
         original_proficiency_level = if_else(original_subject %in% science_eoc & performance_level == 1, "1. Below Basic", original_proficiency_level),
         original_proficiency_level = if_else(original_subject %in% science_eoc & performance_level == 2, "2. Basic", original_proficiency_level),
         original_proficiency_level = if_else(original_subject %in% science_eoc & performance_level == 3, "3. Proficient", original_proficiency_level),
@@ -61,11 +61,11 @@ student_level <- bind_rows(fall_cdf) %>%
         scale_score, enrolled, tested, valid_test, state_student_id = unique_student_id, last_name, first_name, middle_initial,
         grade, race, bhn_group, functionally_delayed, special_ed, economically_disadvantaged, el, el_t1_t2, el_excluded,
         greater_than_60_pct, homebound, absent, did_not_attempt, nullify_flag, residential_facility, semester) %>%
-    mutate_each(funs(as.numeric), system, school, state_student_id) %>% 
+    mutate_at(c("system", "school", "state_student_id"), as.numeric) %>% 
 # Drop excluded records
     filter(!is.na(system)) %>%
     filter(!grade == 13) %>%
-    filter(!(system == 981 | system > 1000)) %>%
+    filter(!(school == 981 | system > 1000)) %>%
 # Apply testing flag hierarchy (5.2.1)
     mutate(
     # Absent students have a missing proficiency and tested value
@@ -73,7 +73,8 @@ student_level <- bind_rows(fall_cdf) %>%
         proficiency_level = if_else(absent == 1, NA_character_, proficiency_level),
     # EL Excluded students with missing proficiency are not considered tested
         proficiency_level = if_else(el_excluded == 1, NA_character_, proficiency_level),
-        tested = if_else(el_excluded == 1 & subject %in% c(math_eoc, science_eoc), 0, tested),
+        tested = if_else(el_excluded == 1 & is.na(original_proficiency_level) & subject %in% c(math_eoc, science_eoc), 0, tested),
+        tested = if_else(el_excluded == 1 & !is.na(original_proficiency_level) & subject %in% c(math_eoc, science_eoc), 1, tested),
     # Proficiency modified to missing if nullify or did not attempt
         proficiency_level = if_else(nullify_flag == 1 | did_not_attempt == 1, NA_character_, proficiency_level),
     # Students taking MSAA are considered special education (5.5)
@@ -97,15 +98,15 @@ dedup <- student_level %>%
     # For students with multiple records across test types, MSAA has priority, then TCAP/EOC
     mutate(test_priority = ifelse(test == "MSAA", 2, 1)) %>%
     group_by(state_student_id, subject, grade) %>%
-    mutate(temp = max(test_priority, na.rm = TRUE)) %>%
-    filter(!test_priority != temp & !is.na(temp)) %>%
+    mutate(temp = max(test_priority)) %>%
+    filter(test_priority == temp | is.na(temp)) %>%
     select(-test_priority, -temp) %>%
     ungroup() %>%
     # For students with multiple records within the same test, take highest proficiency level
     mutate(prof_priority = as.integer(substr(proficiency_level, 1, 1))) %>%
     group_by(state_student_id, subject, grade, test) %>%
     mutate(temp = max(prof_priority, na.rm = TRUE)) %>%
-    filter(!(prof_priority != temp & !is.na(temp))) %>%
+    filter(prof_priority == temp | temp == -Inf) %>%
     select(-prof_priority, -temp) %>%
     ungroup() %>%
     # For students with multiple test records with the same proficiency across administrations, take the most recent
@@ -113,8 +114,8 @@ dedup <- student_level %>%
         semester_priority = if_else(test == "EOC" & semester == "Fall", 2, semester_priority),
         semester_priority = if_else(test == "EOC" & semester == "Summer", 1, semester_priority)) %>%
     group_by(state_student_id, subject, grade, test) %>%
-    mutate(temp = max(semester_priority, na.rm = TRUE)) %>%
-    filter(!semester_priority != temp & !is.na(temp)) %>%
+    mutate(temp = max(semester_priority)) %>%
+    filter(semester_priority == temp | is.na(temp)) %>%
     select(-semester_priority, -temp) %>%
     ungroup() %>%
     # Valid test if there is a proficiency level
@@ -123,8 +124,8 @@ dedup <- student_level %>%
 output <- dedup %>%
     filter(!is.na(state_student_id)) %>%
     select(system, system_name, school, school_name, test, original_subject, subject, original_proficiency_level, proficiency_level,
-        scale_score, enrolled, tested, valid_test, state_student_id, last_name, first_name, grade, race, bhn_group,
-        functionally_delayed, special_ed, economically_disadvantaged, el, el_t1_t2, el_excluded, greater_than_60_pct,
+        scale_score, enrolled, tested, valid_test, state_student_id, last_name, first_name, middle_initial, grade, race,
+        bhn_group, special_ed, economically_disadvantaged, el, el_t1_t2, el_excluded, greater_than_60_pct,
         homebound, absent, did_not_attempt, nullify_flag, residential_facility) %>%
     arrange(system, school, state_student_id)
     # Drop duplicates for match
