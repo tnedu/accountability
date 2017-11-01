e16584dbbce05f61aecc6844d7346171d25c756e
Alexander Poon <alex.poon@tn.gov>
2017-11-01 12:04:22 -0500

    report card district determs

---
+++ 2017-accountability/report-card/district_determs.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+determs <- haven::read_dta("K:/ORP_accountability/projects/2017_district_accountability/final_determinations_master_JW_10232017.dta") %>%
+    transmute(`District Number` = system,
+        `Final Determination: Name` = final_determination,
+        `Final Determination: Definition` = "",
+        `All Students Determination: Name` = achievement_determination,
+        `All Students Determination: Definition` = "",
+        `Subgroup Determination: Name` = gap_closure_determination,
+        `Subgroup Determination: Definition` = "")
+
+write_csv(determs, "K:/ORP_accountability/data/2017_final_accountability_files/Report Card/Accountability File Layout.csv", na = "")
