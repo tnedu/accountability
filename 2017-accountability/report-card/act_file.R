d929a710c2be6996ff75316c8560a63432f738a7
Alexander Poon <alex.poon@tn.gov>
2017-12-15 14:07:27 -0600

    add ACT >21 counts by gender

--- 2017-accountability/report-card/act_file.R
+++ 2017-accountability/report-card/act_file.R
@@ -9,8 +9,8 @@ act_state <- read_dta("K:/ORP_accountability/data/2017_ACT/ACT_state2018_appeals
         `CRB % Eng` = pct_met_CRB_english, `CRB % Math` = pct_met_CRB_math, `CRB % Reading` = pct_met_CRB_reading,
         `CRB % Sci` = pct_met_CRB_science, `CRB % All Four` = pct_met_All4_CRB,
         n_21_or_higher = n_21_orhigher, pct_21_or_higher = pct_21_orhigher,
-        n_female_21_or_higher = NA, pct_female_21_or_higher = female_pct21orhigher,
-        n_male_21_or_higher = NA, pct_male_21_or_higher = male_pct21orhigher,
+        n_female_21_or_higher = female_n_21_orhigher, pct_female_21_or_higher = female_pct21orhigher,
+        n_male_21_or_higher = male_n_21_orhigher, pct_male_21_or_higher = male_pct21orhigher,
         n_gender_missing_21_or_higher = nmissing_21_orhigher, pct_gender_missing_21_or_higher = 0, pct_gender_missing_below_19 = "")
 
 act_district <- read_dta("K:/ORP_accountability/data/2017_ACT/ACT_district2018_appeals.dta") %>%
@@ -21,8 +21,8 @@ act_district <- read_dta("K:/ORP_accountability/data/2017_ACT/ACT_district2018_a
         `CRB % Eng` = pct_met_CRB_english, `CRB % Math` = pct_met_CRB_math, `CRB % Reading` = pct_met_CRB_reading,
         `CRB % Sci` = pct_met_CRB_science, `CRB % All Four` = pct_met_All4_CRB,
         n_21_or_higher = n_21_orhigher, pct_21_or_higher = pct_21_orhigher,
-        n_female_21_or_higher = NA, pct_female_21_or_higher = female_pct21orhigher,
-        n_male_21_or_higher = NA, pct_male_21_or_higher = male_pct21orhigher,
+        n_female_21_or_higher = female_n_21_orhigher, pct_female_21_or_higher = female_pct21orhigher,
+        n_male_21_or_higher = male_n_21_orhigher, pct_male_21_or_higher = male_pct21orhigher,
         n_gender_missing_21_or_higher = nmissing_21_orhigher, pct_gender_missing_21_or_higher = 0, pct_gender_missing_below_19 = "")
 
 act_school <- read_dta("K:/ORP_accountability/data/2017_ACT/ACT_school2018_appeals.dta") %>%
@@ -33,11 +33,11 @@ act_school <- read_dta("K:/ORP_accountability/data/2017_ACT/ACT_school2018_appea
         `CRB % Eng` = pct_met_CRB_english, `CRB % Math` = pct_met_CRB_math, `CRB % Reading` = pct_met_CRB_reading,
         `CRB % Sci` = pct_met_CRB_science, `CRB % All Four` = pct_met_All4_CRB,
         n_21_or_higher = n_21_orhigher, pct_21_or_higher = pct_21_orhigher,
-        n_female_21_or_higher = NA, pct_female_21_or_higher = female_pct21orhigher,
-        n_male_21_or_higher = NA, pct_male_21_or_higher = male_pct21orhigher,
+        n_female_21_or_higher = female_n_21_orhigher, pct_female_21_or_higher = female_pct21orhigher,
+        n_male_21_or_higher = male_n_21_orhigher, pct_male_21_or_higher = male_pct21orhigher,
         n_gender_missing_21_or_higher = nmissing_21_orhigher, pct_gender_missing_21_or_higher = 0, pct_gender_missing_below_19 = "")
 
 act_all <- bind_rows(act_state, act_district, act_school) %>%
     arrange(District_ID, School_ID)
 
-write_csv(act_all, path = "K:/ORP_accountability/data/2017_final_accountability_files/Report Card/ACT_File_2016_17.csv")
+write_csv(act_all, path = "K:/ORP_accountability/data/2017_final_accountability_files/Report Card/ACT_File_2016_17_dec15.csv")
