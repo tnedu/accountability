c2da262491f73f295a01151ae245969e645d8aa9
Alexander Poon <alex.poon@tn.gov>
2018-08-18 14:18:27 -0500

    assessment files with 50/60 percent enrollment removed for district accountability

---
+++ 2018-accountability/district/2017_district_assessment_file_60_pct.R
@@ -0,0 +1,111 @@
+library(acct)
+library(haven)
+library(tidyverse)
+
+math_eoc <- c("Algebra I", "Algebra II", "Geometry", "Integrated Math I", "Integrated Math II", "Integrated Math III")
+english_eoc <- c("English I", "English II", "English III")
+science_eoc <- c("Biology I", "Chemistry")
+
+el_t3_t4 <- readxl::read_excel("N:/ORP_accountability/projects/Jessica/Report Card files/ELB_T3_T4.xls") %>%
+    transmute(id = STUDENT_KEY, EL_T3_T4 = 1) %>%
+    distinct()
+
+student_level <- read_dta("N:/ORP_accountability/projects/2017_student_level_file/state_student_level_2017_JP_final_10192017.dta") %>%
+    mutate(grade = if_else(is.na(grade), 0, grade)) %>%
+    filter(grade %in% c(0, 3:12), greater_than_60_pct == "Y") %>%
+    left_join(el_t3_t4, by = "id") %>%
+    mutate(EL_T3_T4 = if_else(is.na(EL_T3_T4), 0, EL_T3_T4)) %>%
+# Proficiency and subgroup indicators for collapse
+    rename(BHN = bhn_group, ED = economically_disadvantaged, SWD = special_ed, EL = ell, EL_T1_T2 = ell_t1t2) %>%
+    mutate(year = 2017,
+        grade = if_else(is.na(grade), 0, grade),
+        test = if_else(test %in% c("MSAA", "ALT_SCI"), "MSAA/Alt-Science", test),
+        test = if_else(test == "TNReady" & original_subject %in% c(math_eoc, english_eoc, science_eoc, "US History"), "EOC", test),
+        original_subject = if_else(test == "MSAA", subject, original_subject),
+        n_below = if_else(performance_level %in% c("1. Below", "1. Below Basic"), 1L, NA_integer_),
+        n_approaching = if_else(performance_level %in% c("2. Approaching", "2. Basic"), 1L, NA_integer_),
+        n_on_track = if_else(performance_level %in% c("3. On Track", "3. Proficient"), 1L, NA_integer_),
+        n_mastered = if_else(performance_level %in% c("4. Mastered", "4. Advanced"), 1L, NA_integer_),
+        All = 1L,
+        Asian = race == "Asian",
+        Black = race == "Black or African American",
+        Hispanic = race == "Hispanic",
+        Hawaiian = race == "Native Hawaiian or Pacific Islander",
+        Native = race == "American Indian or Alaskan Native",
+        White = race == "White",
+        T1234 = EL_T1_T2 == 1 | EL_T3_T4 == 1,
+        EL_T1234 = EL == 1 | T1234 == 1,
+        Non_BHN = BHN == 0,
+        Non_ED = ED == 0,
+        Non_SWD = SWD == 0,
+        Non_EL = EL_T1234 == 0,
+        Super = (BHN == 1 | ED == 1 | SWD == 1 | EL_T1234 == 1)) %>%
+    mutate_at(c("Asian", "Black", "Hispanic", "Hawaiian", "Native", "White", "BHN", "ED", "SWD",
+        "EL", "T1234", "EL_T1234", "Non_BHN", "Non_ED", "Non_SWD", "Non_EL", "Super"), as.integer) %>%
+# EL Excluded are counted as tested and enrolled but do not receive a proficiency level
+    mutate(original_perfomance_level = if_else(ell_excluded == 1, "", original_performance_level))
+
+collapse <- tibble()
+
+# Collapse proficiency by subject and subgroup
+for (s in c("All", "Asian", "Black", "Hispanic", "Hawaiian", "Native", "White", "BHN", "ED", "SWD",
+    "EL", "T1234", "EL_T1234", "Non_BHN", "Non_ED", "Non_SWD", "Non_EL", "Super")) {
+    
+    collapse <- student_level %>%
+        filter_(paste(s, "== 1L")) %>%
+        group_by(year, system, test, original_subject) %>%
+        summarise_at(c("enrolled", "tested", "valid_test", "n_below", "n_approaching", "n_on_track", "n_mastered"), sum, na.rm = TRUE) %>%
+        mutate(subgroup = s, grade = "All Grades") %>%
+        bind_rows(collapse, .)
+    
+    collapse <- student_level %>%
+        mutate(grade = as.character(grade)) %>%
+        filter_(paste(s, "== 1L")) %>%
+        group_by(year, system, test, original_subject, grade) %>%
+        summarise_at(c("enrolled", "tested", "valid_test", "n_below", "n_approaching", "n_on_track", "n_mastered"), sum, na.rm = TRUE) %>%
+        mutate(subgroup = s) %>%
+        bind_rows(collapse, .)
+    
+}
+
+assessment_2017 <- collapse %>%
+    rename(valid_tests = valid_test, subject = original_subject) %>%
+    mutate(pct_approaching = if_else(valid_tests != 0, round5(100 * n_approaching/valid_tests, 1), NA_real_),
+        pct_on_track = if_else(valid_tests != 0, round5(100 * n_on_track/valid_tests, 1), NA_real_),
+        pct_mastered = if_else(valid_tests != 0, round5(100 * n_mastered/valid_tests, 1), NA_real_),
+        pct_below = if_else(valid_tests != 0, round5(100 - pct_approaching - pct_on_track - pct_mastered, 1), NA_real_),
+        pct_on_mastered = if_else(valid_tests != 0, round5(100 * (n_on_track + n_mastered)/valid_tests, 1), NA_real_),
+    # Fix % B/A/O if there are no n_B/A/O
+        pct_approaching = if_else(pct_below != 0 & n_below == 0, 100 - pct_on_track - pct_mastered, pct_approaching),
+        pct_below = if_else(pct_below != 0 & n_below == 0, 0, pct_below),
+        pct_on_track = if_else(pct_approaching != 0 & n_approaching == 0, 100 - pct_mastered, pct_on_track),
+        pct_approaching = if_else(pct_approaching != 0 & n_approaching == 0, 0, pct_approaching),
+        subgroup = case_when(
+            subgroup == "All" ~ "All Students",
+            subgroup == "Black" ~ "Black or African American",
+            subgroup == "BHN" ~ "Black/Hispanic/Native American",
+            subgroup == "ED" ~ "Economically Disadvantaged",
+            subgroup == "EL" ~ "English Learners",
+            subgroup == "T1234" ~ "English Learner Transitional 1-4",
+            subgroup == "EL_T1234" ~ "English Learners with Transitional 1-4",
+            subgroup == "Hawaiian" ~ "Native Hawaiian or Other Pacific Islander",
+            subgroup == "Native" ~ "American Indian or Alaska Native",
+            subgroup == "Non_BHN" ~ "Non-Black/Hispanic/Native American",
+            subgroup == "Non_ED" ~ "Non-Economically Disadvantaged",
+            subgroup == "Non_EL" ~ "Non-English Learners/Transitional 1-4",
+            subgroup == "Non_SWD" ~ "Non-Students with Disabilities",
+            subgroup == "Super" ~ "Super Subgroup",
+            subgroup == "SWD" ~ "Students with Disabilities",
+            TRUE ~ subgroup
+        )
+    ) %>%
+    transmute(year, system, test, subject, grade = if_else(grade == "0", "Missing Grade", grade),
+        subgroup, enrolled, tested, valid_tests,
+        n_below, n_approaching, n_on_track, n_mastered,
+        pct_below, pct_approaching, pct_on_track, pct_mastered, pct_on_mastered
+    ) %>%
+    arrange(subject, grade, subgroup) %>%
+    filter(!(subject == "Science" & grade %in% 3:4))
+
+# Output file
+write_csv(assessment_2017, "N:/ORP_accountability/data/2018_final_accountability_files/2017_district_assessment_file_60_pct.csv", na = "")
