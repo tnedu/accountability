22bd2a5342f9c26d28d5124532b7f052b500edba
Alexander Poon <alex.poon@tn.gov>
2018-10-29 19:17:35 -0500

    rerun absenteeism targets with primary enrollments only

--- 2018-accountability/targets/absenteeism.R
+++ 2018-accountability/targets/absenteeism.R
@@ -1,18 +1,18 @@
 library(acct)
 library(tidyverse)
 
-district <- read_csv("N:/ORP_accountability/data/2018_chronic_absenteeism/district_chronic_absenteeism.csv", col_types = "iicccddd") %>%
+district <- read_csv("N:/ORP_accountability/data/2018_chronic_absenteeism/district_chronic_absenteeism_primary_enrollment_only.csv", col_types = "iicccddd") %>%
     mutate(
         AMO_target = amo_reduction(n_students, pct_chronically_absent),
         AMO_target_double = amo_reduction(n_students, pct_chronically_absent, double = TRUE)
     )
 
-write_csv(district, "N:/ORP_accountability/projects/2019_amo/absenteeism_targets_district.csv", na = "")
+write_csv(district, "N:/ORP_accountability/projects/2019_amo/absenteeism_targets_district_primary_enrollment.csv", na = "")
 
-school <- read_csv("N:/ORP_accountability/data/2018_chronic_absenteeism/school_chronic_absenteeism.csv", col_types = "iicicccddd") %>%
+school <- read_csv("N:/ORP_accountability/data/2018_chronic_absenteeism/school_chronic_absenteeism_primary_enrollment_only.csv", col_types = "iicicccddd") %>%
     mutate(
         AMO_target = amo_reduction(n_students, pct_chronically_absent),
         AMO_target_double = amo_reduction(n_students, pct_chronically_absent, double = TRUE)
     )
 
-write_csv(school, "N:/ORP_accountability/projects/2019_amo/absenteeism_targets_school.csv", na = "")
+write_csv(school, "N:/ORP_accountability/projects/2019_amo/absenteeism_targets_school_primary_enrollment.csv", na = "")
