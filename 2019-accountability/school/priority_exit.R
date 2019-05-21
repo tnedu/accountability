624525a7de4c4cbe35e73d96c87b8d93cd07abce
Alexander Poon <alexander.poon@vanderbilt.edu>
2019-05-21 13:35:31 -0500

    check csi for exit

--- 2019-accountability/school/priority_exit.R
+++ 2019-accountability/school/priority_exit.R
@@ -1,8 +1,8 @@
 library(acct)
 library(tidyverse)
 
-priority <- read_csv("N:/ORP_accountability/projects/2018_school_accountability/priority.csv") %>%
-    select(system, school, priority)
+priority_csi <- read_csv("N:/ORP_accountability/projects/2018_school_accountability/priority.csv") %>%
+    transmute(system, school, priority_csi = priority == 1 | comprehensive_support == 1)
 
 tvaas <- readxl::read_excel("N:/ORP_accountability/data/2019_tvaas/School Composite Level.xlsx") %>%
     transmute(
@@ -22,7 +22,7 @@ tvaas_prior <- readxl::read_excel("N:/ORP_accountability/data/2018_tvaas/School 
 
 priority_exit <- read_csv("N:/ORP_accountability/data/2019_final_accountability_files/school_accountability_file.csv") %>%
     filter(indicator == "Achievement", subgroup == "All Students") %>%
-    left_join(priority, by = c("system", "school")) %>%
+    left_join(priority_csi, by = c("system", "school")) %>%
     left_join(tvaas, by = c("system", "school")) %>%
     left_join(tvaas_prior, by = c("system", "school")) %>%
     group_by(pool) %>%
@@ -35,7 +35,7 @@ priority_exit <- read_csv("N:/ORP_accountability/data/2019_final_accountability_
         ),
         percentile = round5(100 * rank/denom, 1),
         percentile_prior = round5(100 * rank_prior/denom, 1),
-        priority_exit = as.integer(priority == 1 & 
+        priority_exit = as.integer(priority_csi &
             (percentile > 10 & percentile_prior > 10 |
              percentile > 15 |
              literacy %in% 4:5 & numeracy %in% 4:5 & literacy_prior %in% 4:5 & numeracy_prior %in% 4:5)
@@ -43,9 +43,9 @@ priority_exit <- read_csv("N:/ORP_accountability/data/2019_final_accountability_
     ) %>%
     ungroup() %>%
     select(
-        system, system_name, school, school_name, pool, designation_ineligible, priority,
+        system, system_name, school, school_name, pool, designation_ineligible, priority_csi,
         metric, rank, percentile, metric_prior, rank_prior, percentile_prior, 
-        literacy, numeracy, priority_exit
+        literacy, numeracy, literacy_prior, numeracy_prior, priority_exit
     )
 
 write_csv(priority_exit, "N:/ORP_accountability/projects/2019_school_accountability/priority_exit.csv", na = "")
