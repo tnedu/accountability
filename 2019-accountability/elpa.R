98694225fead64410c1c439f0162cb6bd614d842
Alexander Poon <alexander.poon@vanderbilt.edu>
2019-06-13 14:17:11 -0500

    match and split elpa

--- 2019-accountability/elpa.R
+++ 2019-accountability/elpa.R
@@ -7,7 +7,7 @@ prior <- read_csv("N:/ORP_accountability/data/2018_ELPA/wida_growth_standard_stu
     select(student_id, prof_composite_18 = prof_composite, prof_composite_17)
 
 # Demographic file
-demographics <- read_csv("N:/Assessment_Data Returns/TCAP_End-of-Course/2018-19/Demographic Files/fall_eoc_demographics_snapshot_20181208.csv") %>%
+demographics <- read_csv("N:/TNReady/2018-19/spring/demographics/spring_2019_assessment_demographics_combined_pull_20190610.csv") %>%
     # Student IDs should be 7 digits
     filter(str_length(student_key) == 7) %>%
     transmute(
@@ -35,7 +35,7 @@ demographics <- read_csv("N:/Assessment_Data Returns/TCAP_End-of-Course/2018-19/
         EL = TRUE
     )
 
-wida <- read_csv("N:/Assessment_Data Returns/ACCESS for ELs and ALT/2018-19/TN_Summative_StudRR_File_06032019.csv", guess_max = 20000) %>%
+wida <- read_csv("N:/Assessment_Data Returns/ACCESS for ELs and ALT/2018-19/TN_Summative_StudRR_File_2019-06-07.csv", guess_max = 20000) %>%
     clean_names() %>%
     filter(
         reported_mode != "Mixed",
@@ -76,10 +76,10 @@ wida <- read_csv("N:/Assessment_Data Returns/ACCESS for ELs and ALT/2018-19/TN_S
     mutate(EL = TRUE) %>%
 # Deduplicate by keeping highest composite scale score, then highest literacy scale score
     group_by(student_id) %>%
-    mutate(max_comp = max(prof_composite, na.rm = TRUE)) %>%
-    filter(prof_composite == max_comp) %>%
-    mutate(max_lit = max(prof_literacy, na.rm = TRUE)) %>%
-    filter(prof_literacy == max_lit) %>%
+    mutate(max_comp = max(scale_score_composite, na.rm = TRUE)) %>%
+    filter(scale_score_composite == max_comp) %>%
+    mutate(max_lit = max(scale_score_literacy, na.rm = TRUE)) %>%
+    filter(scale_score_literacy == max_lit) %>%
     ungroup() %>%
 # Deduplicate by non-missing race/ethnicity and grade
     group_by(student_id) %>%
@@ -102,17 +102,17 @@ wida <- read_csv("N:/Assessment_Data Returns/ACCESS for ELs and ALT/2018-19/TN_S
     mutate(
         exit_denom = not_na(prof_composite) & not_na(prof_literacy),
         exit = prof_composite >= 4.2 & prof_literacy >= 4,
-        growth_standard_denom = not_na(prof_composite) & not_na(prof_composite_17),
+        growth_standard_denom = not_na(prof_composite) & not_na(prof_composite_18),
         growth_standard_1yr = case_when(
-            is.na(prof_composite_17) ~ NA_real_,
-            prof_composite_17 <= 1.4 ~ 1.3,
-            prof_composite_17 <= 1.9 ~ 0.7,
-            prof_composite_17 <= 2.4 ~ 0.8,
-            prof_composite_17 <= 2.9 ~ 0.7,
-            prof_composite_17 <= 3.4 ~ 0.4,
-            prof_composite_17 <= 3.9 ~ 0.5,
-            prof_composite_17 <= 4.4 ~ 0.4,
-            prof_composite_17 <= 4.9 ~ 0.2
+            is.na(prof_composite_18) ~ NA_real_,
+            prof_composite_18 <= 1.4 ~ 1.3,
+            prof_composite_18 <= 1.9 ~ 0.7,
+            prof_composite_18 <= 2.4 ~ 0.8,
+            prof_composite_18 <= 2.9 ~ 0.7,
+            prof_composite_18 <= 3.4 ~ 0.4,
+            prof_composite_18 <= 3.9 ~ 0.5,
+            prof_composite_18 <= 4.4 ~ 0.4,
+            prof_composite_18 <= 4.9 ~ 0.2
         ),
         growth_standard_2yr = case_when(
             is.na(prof_composite_17) ~ NA_real_,
@@ -138,17 +138,31 @@ wida <- read_csv("N:/Assessment_Data Returns/ACCESS for ELs and ALT/2018-19/TN_S
         ),
         met_growth_standard = case_when(
             growth_standard_denom == 0 ~ NA_integer_,
-            round5(prof_composite - prof_composite_17, 1) >= growth_standard_1yr | prof_composite >= growth_standard_2yr ~ 1L,
+            round5(prof_composite - prof_composite_18, 1) >= growth_standard_1yr | prof_composite >= growth_standard_2yr ~ 1L,
             TRUE ~ 0L
         ),
-        met_growth_standard = if_else(growth_standard_denom == 1L & exit == 1L, 1L, met_growth_standard)
+        met_growth_standard = if_else(growth_standard_denom & exit, 1L, met_growth_standard)
     )
 
+# Export Student Level File
 wida %>%
     mutate_at(vars(BHN, Hispanic, Black, Native, HPI, Asian, White, ED, SWD, EL, exit_denom, exit, growth_standard_denom), as.integer) %>%
     select(-max_comp, -max_lit, -reported_race) %>%
     write_csv("N:/ORP_accountability/data/2019_ELPA/wida_growth_standard_student.csv", na = "")
 
+# Split Student Level File
+district_numbers <- unique(wida$system)
+
+wida %>%
+    mutate_at(vars(BHN, Hispanic, Black, Native, HPI, Asian, White, ED, SWD, EL, exit_denom, exit, growth_standard_denom), as.integer) %>%
+    select(-max_comp, -max_lit, -reported_race) %>%
+    split(., .$system) %>%
+    walk2(., district_numbers, ~ write_csv(.x, path = paste0("N:/ORP_accountability/data/2019_ELPA/split/", .y, "_ACCESSStudentLevelFile_13Jun2018.csv"), na = ""))
+
+wida <- wida %>%
+    mutate_at(c("ED", "SWD"), ~ if_else(is.na(.), FALSE, .))
+
+# School Level File
 growth_standard_school <- map_dfr(
     .x = list(
         mutate(wida, subgroup = "All Students"),
@@ -188,8 +202,15 @@ growth_standard_school <- map_dfr(
 ) %>%
     arrange(system, school, subgroup)
 
+# Export School Level File
 write_csv(growth_standard_school, "N:/ORP_accountability/data/2019_ELPA/wida_growth_standard_school.csv", na = "")
 
+# Split School Level File
+growth_standard_school %>%
+    split(., .$system) %>%
+    walk2(., district_numbers, ~ write_csv(.x, path = paste0("N:/ORP_accountability/data/2019_ELPA/split/", .y, "_ACCESSSchoolLevelFile_13Jun2018.csv"), na = ""))
+
+# District Level File
 growth_standard_district <- map_dfr(
     .x = list(
         mutate(wida, subgroup = "All Students"),
@@ -229,4 +250,53 @@ growth_standard_district <- map_dfr(
 ) %>%
     arrange(system, subgroup)
 
+# Export District Level File
 write_csv(growth_standard_district, "N:/ORP_accountability/data/2019_ELPA/wida_growth_standard_district.csv", na = "")
+
+# Split District Level File
+growth_standard_district %>%
+    split(., .$system) %>%
+    walk2(., district_numbers, ~ write_csv(.x, path = paste0("N:/ORP_accountability/data/2019_ELPA/split/", .y, "_ACCESSDistrictLevelFile_13Jun2018.csv"), na = ""))
+
+# State Level File
+growth_standard_state <- map_dfr(
+    .x = list(
+        mutate(wida, subgroup = "All Students"),
+        filter(wida, Native) %>% mutate(subgroup = "American Indian or Alaska Native"),
+        filter(wida, Asian) %>% mutate(subgroup = "Asian"),
+        filter(wida, Black) %>% mutate(subgroup = "Black or African American"),
+        filter(wida, BHN) %>% mutate(subgroup = "Black/Hispanic/Native American"),
+        filter(wida, ED) %>% mutate(subgroup = "Economically Disadvantaged"),
+        filter(wida, EL) %>% mutate(subgroup = "English Learners with Transitional 1-4"),
+        filter(wida, Hispanic) %>% mutate(subgroup = "Hispanic"),
+        filter(wida, HPI) %>% mutate(subgroup = "Native Hawaiian or Other Pacific Islander"),
+        filter(wida, !ED) %>% mutate(subgroup = "Non-Economically Disadvantaged"),
+        filter(wida, !SWD) %>% mutate(subgroup = "Non-Students with Disabilities"),
+        filter(wida, SWD) %>% mutate(subgroup = "Students with Disabilities"),
+        filter(wida, White) %>% mutate(subgroup = "White")
+    ),
+    .f = function(x) {
+        group_by(x, subgroup) %>%
+        summarise(
+            exit_denom = sum(exit_denom, na.rm = TRUE),
+            n_exit = sum(exit, na.rm = TRUE),
+            growth_standard_denom = sum(growth_standard_denom, na.rm = TRUE),
+            n_met_growth_standard = sum(met_growth_standard, na.rm = TRUE),
+            literacy_average = round5(mean(prof_literacy, na.rm = TRUE), 1),
+            composite_average = round5(mean(prof_composite, na.rm = TRUE), 1)
+        ) %>%
+        ungroup() %>%
+        transmute(
+            subgroup,
+            exit_denom, n_exit,
+            pct_exit = if_else(exit_denom != 0, round5(100 * n_exit/exit_denom, 1), NA_real_),
+            growth_standard_denom, n_met_growth_standard,
+            pct_met_growth_standard = if_else(growth_standard_denom != 0, round5(100 * n_met_growth_standard/growth_standard_denom, 1), NA_real_),
+            literacy_average, composite_average
+        )
+    }
+) %>%
+    arrange(subgroup)
+
+# Export State Level File
+write_csv(growth_standard_state, "N:/ORP_accountability/data/2019_ELPA/wida_growth_standard_state.csv", na = "")
