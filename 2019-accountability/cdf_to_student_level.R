a3d07142af3c868d22ff5d73a6dd594d956af626
Alexander Poon <alexander.poon@vanderbilt.edu>
2019-06-20 14:09:04 -0500

    add spring EOC
    Adds Spring EOC to student level file. Also drops records from
    Alternative, CTE, Adult HS based on current year list.

--- 2019-accountability/cdf_to_student_level.R
+++ 2019-accountability/cdf_to_student_level.R
@@ -1,14 +1,21 @@
 library(acct)
 library(tidyverse)
 
-fall_eoc <- read_csv("N:/ORP_accountability/data/2019_cdf/2019_fall_eoc_cdf.csv",
+fall_eoc <- read_csv("N:/ORP_accountability/data/2019_cdf/2019_fall_eoc_cdf.csv", 
         col_types = "iciccccdiccccdiiiiciiciiciiciiiiiicc") %>%
     mutate(
         test = "EOC",
         semester = "Fall"
     )
 
-cdf <- bind_rows(fall_eoc) %>%
+spring_eoc <- read_csv("N:/ORP_accountability/data/2019_cdf/2019_spring_eoc_cdf.csv",
+        col_types = "iciccccdiccccdiiiiciiciiciiciiiiiicc") %>%
+    mutate(
+        test = "EOC",
+        semester = "Spring"
+    )
+
+cdf <- bind_rows(fall_eoc, spring_eoc) %>%
     mutate(
         performance_level = if_else(performance_level == "On track", "On Track", performance_level),
         absent = reason_not_tested == 1,
@@ -27,7 +34,6 @@ cdf <- bind_rows(fall_eoc) %>%
         original_subject = case_when(
             content_area_code == "ENG" ~ "ELA",
             content_area_code == "MAT" ~ "Math",
-            content_area_code == "SCI" ~ "Science",
             content_area_code == "SOC" ~ "Social Studies",
             content_area_code == "A1" ~ "Algebra I",
             content_area_code == "A2" ~ "Algebra II",
@@ -51,8 +57,7 @@ int_math_systems <- cdf %>%
     group_by(system) %>%
     mutate(temp = max(n)) %>%
     filter(n == temp, content_area_code == "M1") %>%
-    magrittr::extract2("system") %>%
-    as.integer()
+    magrittr::extract2("system")
 
 student_level <- bind_rows(cdf) %>%
     transmute(
@@ -82,45 +87,42 @@ student_level <- bind_rows(cdf) %>%
         functionally_delayed,
         gifted,
         migrant,
-        enrolled_50_pct_district, 
+        enrolled_50_pct_district,
         enrolled_50_pct_school,
         breach_adult, breach_student, irregular_admin, incorrect_grade_subject, refused_to_test, failed_attemptedness,
         absent, not_enrolled, not_scheduled, medically_exempt, residential_facility, tested_alt, did_not_submit
     ) %>%
-    mutate_at(
-        .vars = vars(bhn_group, t1234, el_recently_arrived),
-        .f = as.integer
-    ) %>%
+    mutate_at(vars(bhn_group, t1234, el_recently_arrived), as.integer) %>%
     rowwise() %>%
-    # Apply testing flag hierarchy
+# Apply testing flag hierarchy
     mutate(
         enrolled = case_when(
             any(breach_adult, breach_student, irregular_admin, incorrect_grade_subject, refused_to_test, failed_attemptedness) ~ 0,
             any(not_enrolled, not_scheduled) ~ 0,
             TRUE ~ 1
         ),
-        # EL Recently Arrived students with missing proficiency are not considered tested
+    # EL Recently Arrived students with missing proficiency are not considered tested
         tested = case_when(
             any(breach_adult, breach_student, irregular_admin, incorrect_grade_subject, refused_to_test, failed_attemptedness) ~ 0,
             any(absent, not_enrolled, not_scheduled) ~ 0,
             el_recently_arrived == 1L & is.na(original_performance_level) ~ 0,
             TRUE ~ 1
         ),
-        # EL Recently Arrived students performance level are converted to missing
+    # EL Recently Arrived students performance level are converted to missing
         performance_level = case_when(
             any(breach_adult, breach_student, irregular_admin, incorrect_grade_subject, refused_to_test, failed_attemptedness) ~ NA_character_,
             any(absent, not_enrolled, not_scheduled, medically_exempt, residential_facility, did_not_submit) ~ NA_character_,
             el_recently_arrived == 1 ~ NA_character_,
             TRUE ~ performance_level
         ),
-        # Modify subject for MSAA tests in grades >= 9 (6.8)
+    # Modify subject for MSAA tests in grades >= 9 (6.8)
         subject = case_when(
             original_subject == "Math" & test == "MSAA" & grade >= 9 & system %in% int_math_systems ~ "Integrated Math I",
             original_subject == "Math" & test == "MSAA" & grade >= 9 & !system %in% int_math_systems ~ "Algebra I",
             original_subject == "ELA" & test == "MSAA" & grade >= 9 ~ "English II",
             TRUE ~ subject
         ),
-        # Convert subjects per accountability rules
+    # Convert subjects per accountability rules
         subject = case_when(
             grade %in% 3:8 & original_subject %in% math_eoc ~ "Math",
             grade %in% 3:8 & original_subject %in% english_eoc ~ "ELA",
@@ -131,13 +133,13 @@ student_level <- bind_rows(cdf) %>%
     ungroup()
 
 # Records from Alternative, CTE, Adult HS are dropped from student level
-# cte_alt_adult <- read_csv("N:/ORP_accountability/data/2019_tdoe_provided_files/cte_alt_adult_schools.csv") %>%
-#     transmute(system = as.numeric(DISTRICT_NUMBER), school = as.numeric(SCHOOL_NUMBER))
+cte_alt_adult <- read_csv("N:/ORP_accountability/data/2019_tdoe_provided_files/cte_alt_adult_schools.csv") %>%
+    transmute(system = as.numeric(DISTRICT_NUMBER), school = as.numeric(SCHOOL_NUMBER))
 
 dedup <- student_level %>%
-    # anti_join(cte_alt_adult, by = c("system", "school")) %>%
+    anti_join(cte_alt_adult, by = c("system", "school")) %>%
+# For students with multiple records across test types, MSAA has priority, then EOC, then 3-8
     mutate(
-        # For students with multiple records across test types, MSAA has priority, then EOC, then 3-8
         test_priority = case_when(
             test %in% c("MSAA", "Alt-Social Studies") ~ 3,
             test == "EOC" ~ 2,
@@ -172,7 +174,7 @@ dedup <- student_level %>%
 # For students with multiple test records with the same proficiency across administrations, take the most recent
     mutate(
         semester_priority = case_when(
-            test %in% c("MSAA", "Alt-Science/Social Studies", "Achievement") | (test == "EOC" & semester == "Spring") ~ 2,
+            test %in% c("MSAA", "Alt-Social Studies", "Achievement") | (test == "EOC" & semester == "Spring") ~ 2,
             test == "EOC" & semester == "Fall" ~ 1
         )
     ) %>%
@@ -182,7 +184,7 @@ dedup <- student_level %>%
     select(-semester_priority, -temp) %>%
     ungroup() %>%
 # For students with multiple test records with the same original subject, performance level, scale score
-# Deduplicate by missing race/ethnicity (!)
+# Deduplicate by missing race/ethnicity
     group_by(state_student_id, original_subject, test, performance_level, scale_score, semester) %>%
     mutate(
         n = n(),                           # Tag duplicates by id, subject, test, performance level, scale_score, semester
@@ -192,7 +194,7 @@ dedup <- student_level %>%
     ungroup() %>%
     select(-n, -temp) %>%
 # For students multiple test records with the same original subject, performance level, scale score, demographics
-# Deduplicate for non-missing grade (!)
+# Deduplicate for non-missing grade
     group_by(state_student_id, original_subject, test, performance_level, scale_score, semester, reported_race) %>%
     mutate(
         n = n(),                   # Tag duplicates by id, subject, test, performance, leve, scale_score, semester
@@ -202,7 +204,7 @@ dedup <- student_level %>%
     ungroup() %>%
     select(-n, -temp) %>%
 # Valid test if there is a proficiency level
-    mutate(valid_test = as.integer(!is.na(performance_level)))
+    mutate(valid_test = as.integer(not_na(performance_level)))
 
 # Reassigned schools for accountability
 enrollment <- read_csv("N:/ORP_accountability/data/2019_final_accountability_files/enrollment.csv") %>%
@@ -220,22 +222,22 @@ output <- dedup %>%
 # Percentiles by grade and original subject for 3-8
     group_by(test, original_subject, grade) %>%
     mutate(
-        rank = if_else(!is.na(scale_score), rank(scale_score, ties = "max"), NA_integer_),
-        denom = sum(!is.na(scale_score)),
+        rank = if_else(not_na(scale_score), rank(scale_score, ties = "max"), NA_integer_),
+        denom = sum(not_na(scale_score)),
         percentile = if_else(test == "Achievement", round5(100 * rank/denom, 1), NA_real_)
     ) %>%
 # Percentiles by original subject for EOCs
     group_by(test, original_subject) %>%
     mutate(
-        rank = if_else(!is.na(scale_score), rank(scale_score, ties = "max"), NA_integer_),
-        denom = sum(!is.na(scale_score)),
+        rank = if_else(not_na(scale_score), rank(scale_score, ties = "max"), NA_integer_),
+        denom = sum(not_na(scale_score)),
         percentile = if_else(test == "EOC", round5(100 * rank/denom, 1), percentile)
     ) %>%
     ungroup() %>%
     select(-rank, -denom) %>%
     arrange(system, school, state_student_id) %>%
 # Add system and school for accountability purposes
-    left_join(enrollment, by = c("state_student_id" = "student_id")) %>%
+    left_join(enrollment, by = "state_student_id") %>%
     mutate(
         acct_system = if_else(is.na(acct_system), system, acct_system),
         acct_school = if_else(is.na(acct_school), school, acct_school)
