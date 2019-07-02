1c5bc8b32c29e3d6d3da63383aaf2523d9b9ee63
Alexander Poon <alexander.poon@vanderbilt.edu>
2019-07-02 18:30:09 -0500

    50 percent enrollment modifications
    Check that a student was enrolled 50 percent or more of the school year
    at a school before taking maximum number of ISP days. This ensure we
    don't identify an accountability system/school that has more ISP days
    but represents an enrollment of less than 50 percent. Also remove
    enrollments in CTE, Alternative, Adult HS.

--- 2019-accountability/enrollment.R
+++ 2019-accountability/enrollment.R
@@ -1,23 +1,24 @@
 library(tidyverse)
 
+cte_alt_adult <- read_csv("N:/ORP_accountability/data/2019_tdoe_provided_files/cte_alt_adult_schools.csv") %>%
+    transmute(system = as.numeric(DISTRICT_NUMBER), school = as.numeric(SCHOOL_NUMBER))
+
 dupes <- read_csv("N:/ORP_accountability/data/2019_chronic_absenteeism/student_chronic_absenteeism_Jun17.csv") %>%
     filter(str_length(student_id) == 7) %>%
+    anti_join(cte_alt_adult, by = c("system", "school")) %>%
 # Remove duplicates on all columns
     distinct() %>%
 # Aggregate records for students by school
     group_by(system, school, student_id) %>%
     summarise(isp_days = sum(isp_days, na.rm = TRUE), instructional_calendar_days = first(instructional_calendar_days)) %>%
     ungroup() %>%
-# Look for students with more than one enrollment
-    add_count(student_id) %>%
-    filter(n > 1) %>%
-    select(-n) %>%
-    group_by(student_id) %>%
+    filter(isp_days/instructional_calendar_days >= .5) %>%
 # Identify school with greatest number of ISP days
+    group_by(student_id) %>%
     mutate(temp = max(isp_days)) %>%
     ungroup() %>%
 # Keep only record with greatest number of ISP days if >= half of school year
-    filter(temp == isp_days, isp_days/instructional_calendar_days >= .5) %>%
+    filter(temp == isp_days) %>%
 # If multiple records exist with >= half of school year enrollment, use system and school from assessment record
     add_count(student_id) %>%
     filter(n == 1) %>%
