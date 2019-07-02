37213134a3e2e20838201ffa6f8c3f42380722ca
Alexander Poon <alexander.poon@vanderbilt.edu>
2019-07-02 18:34:41 -0500

    school numbers
    Fix school numbers for two school districts. Also, confirm using
    demographics from demographic file, not MSAA file.

--- 2019-accountability/dictionary/msaa.R
+++ 2019-accountability/dictionary/msaa.R
@@ -7,23 +7,6 @@ math <- read_csv("N:/Assessment_Data Returns/TCAP ALT_ Grades 3-11_MSAA/2018-19/
         unique_student_id = State_Student_ID,
         last_name = LastOrSurname,
         first_name = FirstName,
-        # economically_disadvantaged = if_else(EconomicDisadvantageStatus == "Yes", "Y", "N"),
-        # el = if_else(LEPStatus == "Yes", "Y", "N"),
-        # hispanic = if_else(HispanicOrLatinaEthnicity == "Yes", "Y", "N"),
-        # black = if_else(BlackorAfricanAmerican == "Yes", "Y", "N"),
-        # native_american = if_else(AmericanIndianorAlaskaNative == "Yes", "Y", "N"),
-        # hawaiian_pi = if_else(NativeHawaiianOthPacificIslander == "Yes", "Y", "N"),
-        # asian = if_else(Asian == "Yes", "Y", "N"),
-        # white = if_else(White == "Yes", "Y", "N"),
-        # reported_race = case_when(
-        #     HispanicOrLatinaEthnicity == "Yes" ~ "H",
-        #     BlackorAfricanAmerican == "Yes" ~ "B",
-        #     AmericanIndianorAlaskaNative == "Yes" ~ "I",
-        #     NativeHawaiianOthPacificIslander == "Yes" ~ "P",
-        #     Asian == "Yes" ~ "A",
-        #     White == "Yes" ~ "W",
-        #     TRUE ~ NA_character_
-        # ),
         grade = as.character(Grade),
         original_subject = "Math",
         scale_score = MatScaledScore,
@@ -43,23 +26,6 @@ ela <- read_csv("N:/Assessment_Data Returns/TCAP ALT_ Grades 3-11_MSAA/2018-19/2
         unique_student_id = State_Student_ID,
         last_name = LastOrSurname,
         first_name = FirstName,
-        # economically_disadvantaged = if_else(EconomicDisadvantageStatus == "Yes", "Y", "N"),
-        # el = if_else(LEPStatus == "Yes", "Y", "N"),
-        # hispanic = if_else(HispanicOrLatinaEthnicity == "Yes", "Y", "N"),
-        # black = if_else(BlackorAfricanAmerican == "Yes", "Y", "N"),
-        # native_american = if_else(AmericanIndianorAlaskaNative == "Yes", "Y", "N"),
-        # hawaiian_pi = if_else(NativeHawaiianOthPacificIslander == "Yes", "Y", "N"),
-        # asian = if_else(Asian == "Yes", "Y", "N"),
-        # white = if_else(White == "Yes", "Y", "N"),
-        # reported_race = case_when(
-        #     HispanicOrLatinaEthnicity == "Yes" ~ "H",
-        #     BlackorAfricanAmerican == "Yes" ~ "B",
-        #     AmericanIndianorAlaskaNative == "Yes" ~ "I",
-        #     NativeHawaiianOthPacificIslander == "Yes" ~ "P",
-        #     Asian == "Yes" ~ "A",
-        #     White == "Yes" ~ "W",
-        #     TRUE ~ NA_character_
-        # ),
         grade = as.character(Grade),
         original_subject = "ELA",
         scale_score = ELAScaledScore,
@@ -110,8 +76,12 @@ demographics <- read_csv("N:/TNReady/2018-19/spring/demographics/spring_2019_ass
 names <- read_csv("N:/ORP_accountability/data/2019_final_accountability_files/names.csv")
 
 msaa <- bind_rows(math, ela) %>%
+    mutate(
+        school = if_else(system == 961 & school == 961, 5L, school),
+        school = if_else(system == 963 & school == 963, 5L, school)
+    ) %>%
     left_join(names, by = c("system", "school")) %>%
     left_join(demographics, by = c("system", "school", "unique_student_id")) %>%
     select(system, system_name, school, school_name, everything())
 
-write_csv(msaa, path = "N:/ORP_accountability/data/2019_cdf/2019_msaa_cdf.csv")
+write_csv(msaa, path = "N:/ORP_accountability/data/2019_cdf/2019_msaa_cdf.csv", na = "")
