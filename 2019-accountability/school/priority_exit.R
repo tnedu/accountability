92eca269c7a8e8ef69690e41b451790aad638cc6
Alexander Poon <alexander.poon@vanderbilt.edu>
2019-09-05 12:08:09 -0500

    priority exit denominator
    Priority schools exit if they perform at the 15th percentile based on a
    one-year success rate or at the 10th percentile based on two consecutive
    one-year success rates. Use schools only with success rates for both
    years as the denominator to ensure a consistent comparison for two-year
    success rates.

--- 2019-accountability/school/priority_exit.R
+++ 2019-accountability/school/priority_exit.R
@@ -2,9 +2,9 @@ library(acct)
 library(tidyverse)
 
 priority_csi <- read_csv("N:/ORP_accountability/projects/2018_school_accountability/priority.csv") %>%
-    transmute(system, school, priority_csi = priority == 1 | comprehensive_support == 1)
+    transmute(system, school, priority_csi = as.integer(priority == 1 | comprehensive_support == 1))
 
-tvaas <- readxl::read_excel("N:/ORP_accountability/data/2019_tvaas/School Composite Level.xlsx") %>%
+tvaas <- readxl::read_excel("N:/ORP_accountability/data/2019_tvaas/SAS-NIET School-Wide.xlsx") %>%
     transmute(
         system = as.integer(`District Number`),
         school = as.integer(`School Number`),
@@ -20,26 +20,24 @@ tvaas_prior <- readxl::read_excel("N:/ORP_accountability/data/2018_tvaas/School 
         numeracy_prior = `School-Wide: Numeracy`
     )
 
+## TODO: Add grad pathway in future years
+# grad <- NA
+
 priority_exit <- read_csv("N:/ORP_accountability/data/2019_final_accountability_files/school_accountability_file.csv") %>%
     filter(indicator == "Achievement", subgroup == "All Students") %>%
     left_join(priority_csi, by = c("system", "school")) %>%
     left_join(tvaas, by = c("system", "school")) %>%
     left_join(tvaas_prior, by = c("system", "school")) %>%
-    group_by(pool) %>%
+    mutate(exit_eligible = not_na(metric) & not_na(metric_prior)) %>%
+    group_by(pool, designation_ineligible, exit_eligible) %>%
     mutate(
-        rank = if_else(designation_ineligible == 0 & !is.na(metric), rank(metric, ties = "min"), NA_integer_),
-        rank_prior = if_else(designation_ineligible == 0 & !is.na(metric), rank(metric_prior, ties = "min"), NA_integer_),
-        denom = case_when(
-            pool == "HS" ~ sum(pool == "HS"),
-            pool == "K8" ~ sum(pool == "K8") 
-        ),
+        denom = sum(exit_eligible),
+        rank = if_else(designation_ineligible == 0 & exit_eligible & not_na(metric), rank(metric, ties = "min"), NA_integer_),
+        rank_prior = if_else(designation_ineligible == 0 & exit_eligible & not_na(metric_prior), rank(metric_prior, ties = "min"), NA_integer_),
         percentile = round5(100 * rank/denom, 1),
         percentile_prior = round5(100 * rank_prior/denom, 1),
-        priority_exit = as.integer(priority_csi &
-            (percentile > 10 & percentile_prior > 10 |
-             percentile > 15 |
-             literacy %in% 4:5 & numeracy %in% 4:5 & literacy_prior %in% 4:5 & numeracy_prior %in% 4:5)
-        )
+        tvaas_exit = as.integer(literacy %in% 4:5 & numeracy %in% 4:5 & literacy_prior %in% 4:5 & numeracy_prior %in% 4:5),
+        priority_exit = as.integer(priority_csi & (percentile > 10 & percentile_prior > 10 | percentile > 15 | tvaas_exit == 1L))
     ) %>%
     ungroup() %>%
     select(
