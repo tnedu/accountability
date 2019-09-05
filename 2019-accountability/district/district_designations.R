696b1f819bf63430c3ebdb4c3e5c0249053fb653
Alexander Poon <alexander.poon@vanderbilt.edu>
2019-09-05 12:13:35 -0500

    cutoffs for designations
    Protocol stipulates that districts need an average of 3.1 to earn
    Exemplary; changed from 3 in prior year.

--- 2019-accountability/district/district_designations.R
+++ 2019-accountability/district/district_designations.R
@@ -21,17 +21,17 @@ final <- left_join(all_students_overall, subgroups_overall, by = "system") %>%
         system,
         achievement_average,
         achievement_determination = case_when(
-            achievement_average >= 3 ~ "Exemplary",
-            achievement_average >= 2 ~ "Advancing",
-            achievement_average >= 1 ~ "Satisfactory",
-            achievement_average < 1 ~ "Marginal"
+            achievement_average >= 3.1 ~ "Exemplary",
+            achievement_average >= 2.1 ~ "Advancing",
+            achievement_average >= 1.1 ~ "Satisfactory",
+            achievement_average < 1.1 ~ "Marginal"
         ),
         subgroup_average,
         subgroup_determination = case_when(
-            subgroup_average >= 3 ~ "Exemplary",
-            subgroup_average >= 2 ~ "Advancing",
-            subgroup_average >= 1 ~ "Satisfactory",
-            subgroup_average < 1 ~ "Marginal"
+            subgroup_average >= 3.1 ~ "Exemplary",
+            subgroup_average >= 2.1 ~ "Advancing",
+            subgroup_average >= 1.1 ~ "Satisfactory",
+            subgroup_average < 1.1 ~ "Marginal"
         ),
         overall_average = round5(0.6 * achievement_average + 0.4 * subgroup_average, 1),
         rank = if_else(not_na(overall_average), rank(overall_average, ties.method = "min"), NA_integer_),
@@ -40,10 +40,10 @@ final <- left_join(all_students_overall, subgroups_overall, by = "system") %>%
         met_minimum_performance = as.integer(percentile > 5),
         final_determination = case_when(
             met_minimum_performance == 0 ~ "In Need of Improvement",
-            overall_average >= 3 ~ "Exemplary",
-            overall_average >= 2 ~ "Advancing",
-            overall_average >= 1 ~ "Satisfactory",
-            overall_average < 1 ~ "Marginal"
+            overall_average >= 3.1 ~ "Exemplary",
+            overall_average >= 2.1 ~ "Advancing",
+            overall_average >= 1.1 ~ "Satisfactory",
+            overall_average < 1.1 ~ "Marginal"
         )
     ) %>%
     mutate_at(c("achievement_average", "subgroup_average", "overall_average"), ~ if_else(is.nan(.), NA_real_, .))
